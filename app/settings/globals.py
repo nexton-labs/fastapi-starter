569b9ab0f8deee52eed951742df9fe59076aff65
epicatto <epicatto@hotmail.com>
2020-09-10 12:20:08 -0300

    Upload avatar image
     - Fix lint check.

--- app/settings/globals.py
+++ app/settings/globals.py
@@ -22,5 +22,9 @@ COGNITO_POOL_ID: str = config("COGNITO_POOL_ID", cast=str, default="fake-pool-id
 COGNITO_REGION: str = config("COGNITO_REGION", cast=str, default="us-west-2")
 CANDIDATE_AVATAR_PATH: str = "images/candidates/{candidate_id}/{candidate_id}_avatar"
 AWS_IMG_BUCKET: str = config("AWS_IMG_BUCKET", cast=str, default="fastapi-starter")
-AWS_ACCESS_KEY_ID: str = config("AWS_ACCESS_KEY_ID", cast=str, default="DUMMY_AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY: str = config("AWS_SECRET_ACCESS_KEY", cast=str, default="DUMMY_AWS_ACCESS_KEY_ID")
+AWS_ACCESS_KEY_ID: str = config(
+    "AWS_ACCESS_KEY_ID", cast=str, default="DUMMY_AWS_ACCESS_KEY_ID"
+)
+AWS_SECRET_ACCESS_KEY: str = config(
+    "AWS_SECRET_ACCESS_KEY", cast=str, default="DUMMY_AWS_ACCESS_KEY_ID"
+)
